b83067d3e4a2ca2cf7b1824259b044154d3bceb2
MissTee11 <zizhtarie@gmail.com>
2024-05-19 02:10:55 +0300

    fixed login page

--- php/login.php
+++ php/login.php
@@ -1,4 +1,14 @@
-?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/phppractice/Web Teknolojiler Proje/php/login.css">
+</head>
+<body>
+<img src = "/phppractice/Web Teknolojiler Proje/images/zim.jpg" class = "images">
+
+<?php
 if ($_SERVER["REQUEST_METHOD"]== "POST")
 {
     $email = $_POST['email'];
@@ -9,16 +19,27 @@ if ($_SERVER["REQUEST_METHOD"]== "POST")
 
     if($email===$expectedemail && $password===$expectedpassword)
     {
-        echo "Welcome B221210592!";
-        header("Location:home.html");
+        echo '<div class ="message"> Welcome B221210592!</div>';
+        echo'<script>
+                setTimeout(function() {
+                    window.location.href = "/phppractice/Web%20Teknolojiler%20Proje/home.html";
+                }, 3000); // 3 seconds delay
+              </script>';
         exit();
     }
     else
     {
-        echo"invalid username or password. Please try again";
-        header("Location:index.html");
+        echo'<div class="message error" style="color:grey"> Invalid username or password. Please try again<div>';
+        echo'<script>
+                setTimeout(function() {
+                    window.location.href = "/phppractice/Web%20Teknolojiler%20Proje/index.html";
+                }, 3000); // 3 seconds delay
+              </script>';
         exit();
     }
 }
-
 ?>
+
+
+</body>
+</html>
